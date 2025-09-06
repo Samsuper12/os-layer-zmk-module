876abcd482f8397e570da8ae9bf547a476f75f79
Michael Skorokhodov <teethfall@icloud.com>
2025-09-06 22:50:36 +0300

    add bridge handler

---
+++ src/bridge_handler.c
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2025 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <zmk/bridge.h>
+#include <zmk/os_layer.h>
+
+bridge_Response get_devicset_current_ose_info(const bridge_Request *req) {
+
+  zmk_select_preferred_os_type(
+      (enum zmk_os_type)req->subsystem.os.request_type.set_current_os);
+
+  // return BRIDGE_RESPONSE_OK(set_current_os);
+  return ((bridge_Response{}));
+}
+
+BRIDGE_SUBSYSTEM_HANDLER(os, set_current_os);
