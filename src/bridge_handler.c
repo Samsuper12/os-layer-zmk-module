7163c0ceb3fb332e0fb6c41b9223557a36e2ac9f
Michael Skorokhodov <teethfall@icloud.com>
2025-09-08 14:31:09 +0300

    fix brdige set_current_os

--- src/bridge_handler.c
+++ src/bridge_handler.c
@@ -7,12 +7,12 @@
 #include <zmk/bridge.h>
 #include <zmk/os_layer.h>
 
-bridge_Response get_devicset_current_ose_info(const bridge_Request *req) {
+bridge_Response set_current_os(const bridge_Request *req) {
 
   zmk_select_preferred_os_type(
       (enum zmk_os_type)req->subsystem.os.request_type.set_current_os);
 
-  return BRIDGE_RESPONSE_OK;
+  return BRIDGE_RESPONSE_SIMPLE(true);
 }
 
 BRIDGE_SUBSYSTEM_HANDLER(os, set_current_os);
