cbee34b59a2984367be43c1549543531d5c72205
Michael Skorokhodov <teethfall@icloud.com>
2025-09-22 20:59:36 +0300

    bridge: use a new way to emit keypress

--- src/bridge/handler.c
+++ src/bridge/handler.c
@@ -7,51 +7,23 @@
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/logging/log.h>
-#include <zephyr/sys/util.h>
-#include <zmk/behavior.h>
 #include <zmk/bridge.h>
 #include <zmk/events/position_state_changed.h>
 #include <zmk/os_layer.h>
 #include <zmk/virtual_key_position.h>
 
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
-
-#define OS_SEL_NODE DT_NODELABEL(os_sel)
-static const struct device *os_sel_dev = DEVICE_DT_GET(OS_SEL_NODE);
-
-static struct zmk_behavior_binding behavior = {
-    .behavior_dev = NULL,
-    .param1 = 0,
-    .param2 = 0,
-};
+BRIDGE_DECLARE_BINDING(binding, event, os_sel);
 
 bridge_Response set_current_os(const bridge_Request *req) {
 
   if (!device_is_ready(os_sel_dev)) {
     LOG_ERR("The OS_SEL node cannot be found!");
+    return BRIDGE_RESPONSE_SIMPLE(false);
   }
 
-  struct zmk_behavior_binding_event event = {
-      .position = 0,
-      .timestamp = k_uptime_get(),
-#if IS_ENABLED(CONFIG_ZMK_SPLIT)
-      .source = ZMK_POSITION_STATE_CHANGE_SOURCE_LOCAL,
-#endif
-  };
-  uint16_t os_layer = req->subsystem.os.request_type.set_current_os;
-  behavior.behavior_dev = os_sel_dev;
-  behavior.param1 = os_layer;
-  zmk_select_preferred_os_type(os_layer);
-
-  // In this case, the event (keypress) should be sent to all parts of the
-  // keyboard.
-  // Common events such as 'raise_' cannot do this.
-  zmk_split_central_invoke_behavior(ZMK_POSITION_STATE_CHANGE_SOURCE_LOCAL,
-                                    &behavior, event, true);
-  k_msleep(2);
-  event.timestamp = k_uptime_get();
-  zmk_split_central_invoke_behavior(ZMK_POSITION_STATE_CHANGE_SOURCE_LOCAL,
-                                    &behavior, event, false);
+  binding.param1 = req->subsystem.os.request_type.set_current_os;
+  bridge_tap_binding(&binding, event);
 
   return BRIDGE_RESPONSE_SIMPLE(true);
 }
